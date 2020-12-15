46dd1190522e87056414de7a2ff2767d5c59e2f2
pondbiggy <73007016+pondbiggy@users.noreply.github.com>
2020-12-15 15:58:40 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,16 @@
 <!DOCTYPE html>
-<html lang='th'> #ใช้สำหรับไทย
+<html lang='th'> 
   <head>
-   <meta charset="UTF-8"> #แสดงชนิดอักขระ
+   <meta charset="UTF-8"> 
 	 <title>ทดสอบ HTML </title>
   <head>
 
   <body>
-	<h1>สวัสดี ศิลปากร</h1>
+    <h1>สวัสดี ศิลปากร</h1>
+    <h2>The href Attribute</h2>
+
+<p>HTML links are defined with the a tag. The link address is specified in the href attribute:</p>
+
+<a href="https://www.cp.su.ac.th/curricula/Bachelor-of-Science-Program-in-Data-Science">Visit W3Schools</a>
   </body>
 <html>
